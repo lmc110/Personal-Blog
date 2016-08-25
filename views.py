52c03c560e7d2995f23e935dd5d3c6ee622e0bf7
luism <luismcampos32@gmail.com>
2016-08-25 13:52:15 -0500

    Set up url handler for index page

--- views.py
+++ views.py
@@ -3,8 +3,11 @@ import models
 
 from init import db, app
 
+
 @app.route('/')
 def index():
+    auth_user = flask.session.get('auth_user', None)
+
     return flask.render_template('index.html')
 
 
