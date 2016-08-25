9afb0672ebe6e1a8a8fc28a3bcc1f3b49130d052
luism <luismcampos32@gmail.com>
2016-08-25 14:31:34 -0500

    Blog posts are stored to database upon posting

--- views.py
+++ views.py
@@ -28,4 +28,34 @@ def handle_login():
         return flask.render_template('login.html', state='bad')
 
 
+@app.route('/logout')
+def handle_logout():
+    del flask.session['auth_user']
+    return flask.redirect('/')
+
+
+@app.route('/create')
+def create_form():
+    return flask.render_template('create_post.html')
+
+
+@app.route('/create', methods=['POST'])
+def handle_create():
+    title = flask.request.form['title']
+    text = flask.request.form['post-text']
+    #add post to database
+    post = models.Post()
+    post.title = title
+    post.body = text
+    db.session.add(post)
+    db.session.commit()
+    pid = post.id
+    return flask.redirect(flask.url_for('post_page', pid=pid), code=303)
+
+
+@app.route('/posts/<int:pid>')
+def post_page(pid):
+    post = models.Post.query.get(pid)
+    return flask.render_template('post.html', post=post)
+
 
