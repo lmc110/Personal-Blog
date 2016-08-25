5a58844c9a003f776fae1781d16de653b1d322ea
luism <luismcampos32@gmail.com>
2016-08-25 14:04:59 -0500

    Configured admin login

--- views.py
+++ views.py
@@ -11,5 +11,21 @@ def index():
     return flask.render_template('index.html')
 
 
+@app.route('/login')
+def login_form():
+    return flask.render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def handle_login():
+    user = flask.request.form['user']
+    password = flask.request.form['password']
+    if user == 'admin' and password == app.config['ADMIN_PASSWORD']:
+        flask.session['auth_user'] = user
+        return flask.redirect('/', code=303)
+    else:
+        flask.flash('Invalid username or password', 'error')
+        return flask.render_template('login.html', state='bad')
+
 
 
